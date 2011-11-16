b9f55cb944b7e19f36c54388c73c94e943bf0f64
Miles Shang <miles.shang@gmail.com>
2011-11-15 19:17:07 -0500

    Planning comments for movement lines.

--- syntree.js
+++ syntree.js
@@ -5,6 +5,12 @@
  * To get consistent line widths, must align with pixel grid.
  *
  * In order to deal with apostrophes, spaces, brackets, and other non-alphanumeric charaters, make parser ignore any special chars inside of "".
+ *
+ * Movement:
+ * Destinations labelled with _# directly after closing square bracket (e.g. [NP Miles]_1)
+ * Only numeric labels.
+ * Sources indicated with <#> anywhere within text.
+ * Make sure that destination is not parent of source.
  * 
  */
 
